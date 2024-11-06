138b4436a16c3f54c05ee76627141cb3b14ad5d4
ziqian <672699185@qq.com>
2024-11-06 16:22:35 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts" name="App">
+var str = '我爱你祖国'
+</script>
+
+<template>
+  <div class="box">
+    <h1>我是App</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
